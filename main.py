ae8f64de5a9a126b91eceac2ebb34b48683e7beb
Aneesh <aneeshds320@gmail.com>
2023-07-17 21:55:06 +0530

    Add description on the new columns

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.jpeg', width=400)
+    st.image('images/photo.png', width=350)
 
 with col2:
     st.title("Aneesh D S")
     content = """
     Hi, I'm Aneesh, an aspiring python developer. I graduated in Mechanical Engineering 
-    from University of Kerala. I'm currently working in the oil and gas industry. I had always 
-    had a programming aptitude. This website showcases apps that I've coded as part of a 
-    Udemy course by Ardit Sulce.
+    from University of Kerala. I'm currently working in the oil and gas industry. I had 
+    always had a programming aptitude. This website showcases apps that I've coded as 
+    part of an Udemy course by Ardit Sulce.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python.Feel free to contact me!
+"""
+st.write(content2)
